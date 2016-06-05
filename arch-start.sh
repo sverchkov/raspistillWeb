cf05b530bc7f595915f401f712ed05815fc1111a
Yuriy <yuriy.sverchkov@wisc.edu>
2016-06-05 20:29:59 +0000

    ArchLinux start script and matching systemd service file

---
+++ arch-start.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+export PATH=/opt/vc/bin:$PATH
+
+cd /home/alarm/python-env
+source bin/activate
+cd raspistillWeb
+exec ../bin/pserve development.ini > /home/alarm/raspistillWeb.log.txt 2>&1
