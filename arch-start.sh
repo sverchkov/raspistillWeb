90dff380693ca9fa732a0e5cbda737ecdf9c2825
sverchkov <sverchkov@users.noreply.github.com>
2016-06-06 10:33:55 -0500

    Update arch-start.sh
    exec doesn't pass env properly, and isn't needed anyway.

--- arch-start.sh
+++ arch-start.sh
@@ -5,4 +5,4 @@ export PATH=/opt/vc/bin:$PATH
 cd /home/alarm/python-env
 source bin/activate
 cd raspistillWeb
-exec ../bin/pserve development.ini > /home/alarm/raspistillWeb.log.txt 2>&1
+../bin/pserve development.ini > /home/alarm/raspistillWeb.log.txt 2>&1
